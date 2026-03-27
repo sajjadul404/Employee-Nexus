6bc4e4cb2035629032364846ce707fc6ea9124e1
sajjadul404 <sajjadul625@gmail.com>
2026-03-27 16:55:02 +0600

    VARIABLES

--- main.py
+++ main.py
@@ -17,6 +17,15 @@ root.title("Employee Management System")
 root.geometry("950x550")
 root.config(bg="#f8c8dc")
 
+# ---------------- VARIABLES ----------------
+emp_id = tk.StringVar()
+name = tk.StringVar()
+email = tk.StringVar()
+phone = tk.StringVar()
+department = tk.StringVar()
+salary = tk.StringVar()
+search_var = tk.StringVar()   # 🔍 NEW
+
 # ---------------- LOAD DATA ----------------
 fetch_data()
 
